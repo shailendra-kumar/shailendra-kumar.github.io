49e8209cbd63042d25bcd43acca74acc1fba8308
shailendra-kumar <xpsshaily@gmail.com>
2023-11-06 23:29:15 +0530

    Update process_query.php

--- process_query.php
+++ process_query.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $query = $_POST["query"];
 
     // Replace with your email address
-    $to = "your@email.com";
+    $to = "xpsshaily@gmail.com";
     $subject = "New Query from $name";
     $messageBody = "Name: $name\nEmail: $email\n\nQuery:\n$query";
 
